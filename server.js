28c4548b2336448159dece4301e011094aa2bf07
GauravKumar57 <36677667+GauravKumar57@users.noreply.github.com>
2018-03-06 00:03:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,20 +4,93 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles={
+ 'article-one':{
+title:'Article one|Gaurav kumar',
+heading:Article-one,
+date:'march 5,2018',
+content:`<p>
+                This is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my content
+            </p>
+            <p>
+                This is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is myThis is my content
+            </p>
+            <p>
+                This is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my content
+
+            </p>`
+ },
+ 'article-two':{
+     title:'Article two|Gaurav kumar',
+heading:Article-one,
+date:'march 6,2018',
+content:`<p>
+                This is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my content
+            </p>
+            `
+ },
+ 'article-three':{
+      title:'Article three|Gaurav kumar',
+heading:Article-one,
+date:'march 6,2018',
+content:`<p>
+                This is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my contentThis is my content
+            </p>
+            `
+ }
+ 
+ 
+ 
+ 
+};
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=date.heading;
+    var content=data.content;
+
+var htmlTemplate=
+`<html>
+<head>
+    <title>
+        $(title)
+    </title>
+     <link href="/ui/style.css" rel="stylesheet" />
+</head>    
+    <body>
+        <div class='container'>
+        <div>
+            <a href ="/">home</a>
+        </div>
+        <h1>
+            $(heading)
+        </h1>
+        <div>
+             $(date)
+        </div>
+        <div>
+            $(content)
+        </div>
+        </div>
+    </body>
+    
+ </html>`  
+ ;
+ return htmlTemplate;
+ 
+ }
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function(req,res){
+      var articleName=req.params.articleName;
+      res.send(createTemplate(articles[articleName]));
     });
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-    });
-app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
